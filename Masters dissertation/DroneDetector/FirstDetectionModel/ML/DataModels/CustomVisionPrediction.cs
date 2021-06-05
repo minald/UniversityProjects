da20711617324fcee39c0b6c4b233de74d0e5df7
andreyminald <minaldandrey@gmail.com>
2021-06-05 14:28:53 +0300

    Masters dissertation - Renaming projects

---
+++ Masters dissertation/DroneDetector/FirstDetectionModel/ML/DataModels/CustomVisionPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace FirstDetectionModel
+{
+    public class CustomVisionPrediction : IOnnxObjectPrediction
+    {
+        [ColumnName("model_outputs0")]
+        public float[] PredictedLabels { get; set; }
+    }
+}
---
+++ Masters dissertation/DroneDetector/FirstDetectionModel/ML/DataModels/TinyYoloPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace FirstDetectionModel
+{
+    public class TinyYoloPrediction : IOnnxObjectPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels { get; set; }
+    }
+}
